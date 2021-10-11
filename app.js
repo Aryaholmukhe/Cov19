2535eb6c76913c79816a78873e6ff0bab646b251
Arya Holmukhe <aryaholmukhe@gmail.com>
2021-10-11 18:15:20 -0400

    done the right way

--- app.js
+++ app.js
@@ -124,6 +124,10 @@ app.get("/about", (req, res) => {
   res.render("about")
 })
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+ if(port===null||port===""){
+   port = 8000;
+ }
+app.listen(port, function() {
+  console.log('server started on successfully');
 });
